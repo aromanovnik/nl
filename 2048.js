e8bfd3a35031c7b0412ff0681e4683b1ec9938a8
AlekseyRomanov <aromanov@domprog.com>
2024-05-21 16:54:47 +0300

    2048

--- 2048.js
+++ 2048.js
@@ -6,107 +6,105 @@
         [0, 2, 2, 2],
     ];
 
-    const moves = 'U';
-
-    const moveByIndex = (first, second) => {
-        if (!first && !second) {
-            return [first, second];
-        }
-
-        if (first && !second) {
-            return [first, second];
-        }
-
-        if (!first && second) {
-            return [second, first];
-        }
-
-        if (first === second) {
-            return [first + second, 0];
-        }
-
-        return [first, second];
-    };
-
-    const shuffling = (fieldArr, nav) => {
-        switch (nav) {
-            case 'R':
-                for (let i = 0; i < fieldArr.length; i++) {
-                    let horizontally = fieldArr[i].filter((e) => e !== 0);
-                    for (let j = 1; j < horizontally.length; j++) {
-                        const [first, second] = moveByIndex(horizontally[j - 1], horizontally[j]);
-                        horizontally[j - 1] = first;
-                        horizontally[j] = second;
-                    }
-
-                    horizontally = horizontally.filter((e) => e !== 0);
-                    const newField = Array(fieldArr[i].length).fill(0);
-                    newField.splice(fieldArr[i].length - horizontally.length, fieldArr[i].length, ...horizontally);
-
-                    fieldArr[i] = newField;
-                }
-                break;
-            case 'L':
-                for (let i = 0; i < fieldArr.length; i++) {
-                    let horizontally = fieldArr[i].filter((e) => e !== 0);
-
-                    for (let j = horizontally.length - 1; j > 0; j--) {
-                        const [first, second] = moveByIndex(horizontally[j - 1], horizontally[j]);
-                        horizontally[j - 1] = first;
-                        horizontally[j] = second;
-                    }
-
-                    horizontally = horizontally.filter((e) => e !== 0);
-
-                    const newField = Array(fieldArr[i].length).fill(0);
-                    newField.splice(0, horizontally.length, ...horizontally);
-
-                    fieldArr[i] = newField;
-                }
-                break;
-        }
-
-        return fieldArr;
-    }
-
-    const getVertically = (fieldArr) => {
-        let vertically = Array(fieldArr.length).fill([Array(fieldArr.length).fill(0)]);
-        for (let i = 0; i < fieldArr.length; i++) {
-            const t = Array(fieldArr.length).fill(0);
-            for (let j = 0; j < fieldArr[i].length; j++) {
-                t[j] = fieldArr[j][i];
-            }
-            vertically[i] = t;
-        }
-    }
+    const moves = 'U U U';
 
     function solution(field, moves) {
         // U, D, R, L
 
-        moves.split(' ').forEach(move => {
+        const moveByIndex = (first, second) => {
+            if (!first && !second) {
+                return [first, second];
+            }
+
+            if (first && !second) {
+                return [first, second];
+            }
+
+            if (!first && second) {
+                return [second, first];
+            }
+
+            if (first === second) {
+                return [first + second, 0];
+            }
+
+            return [first, second];
+        };
+
+        const shufflingLeft = (line) => {
+            console.log('LINE', line)
+            let horizontally = line.filter((e) => e !== 0);
+
+            for (let j = 0; j < horizontally.length - 1; j++) {
+
+                const [first, second] = moveByIndex(horizontally[j], horizontally[j + 1]);
+                horizontally[j] = first;
+                horizontally[j + 1] = second;
+
+
+                horizontally = horizontally.filter((e) => e !== 0);
+            }
+
+
+            const newField = Array(line.length).fill(0);
+            newField.splice(0, horizontally.length, ...horizontally);
+            line = newField;
+
+            return line;
+        }
+
+        let newField = JSON.parse(JSON.stringify(field));
+
+        const navs = moves.split(' ');
+        for (let i = 0; i < navs.length; i++) {
+            const move = navs[i]
+
             switch (move) {
                 case 'U':
-                    const verticallyU = shuffling(getVertically(field), move);
-                    console.log('verticallyU -> ', verticallyU);
-
+                    for (let j = 0; j < newField.length; j++) {
+                        let col = [];
+                        for (let i = 0; i < 4; i++) {
+                            col.push(newField[i][j]);
+                        }
+                        col = shufflingLeft(col);
+                        for (let i = 0; i < newField.length; i++) {
+                            newField[i][j] = col[i];
+                        }
+                    }
                     break;
                 case 'D':
-                    const verticallyD = shuffling(getVertically(field), move);
-                    console.log('verticallyD -> ', verticallyD);
-
-
+                    for (let j = 0; j < newField.length; j++) {
+                        let col = [];
+                        for (let i = 0; i < newField.length; i++) {
+                            col.push(newField[i][j]);
+                        }
+                        col = shufflingLeft(col.slice().reverse()).reverse();
+                        for (let i = 0; i < 4; i++) {
+                            newField[i][j] = col[i];
+                        }
+                    }
                     break;
                 case 'R':
+                    console.log('111', JSON.parse(JSON.stringify(newField)))
+                    for (let i = 0; i < newField.length; i++) {
+                        newField[i] = shufflingLeft(newField[i].reverse()).reverse()
+                    }
+                    break;
                 case 'L':
-                    field = shuffling(field, move)
+                    for (let i = 0; i < newField.length; i++) {
+                        newField[i] = shufflingLeft(newField[i])
+                    }
                     break;
             }
-        });
 
+            console.log(JSON.parse(JSON.stringify(newField)))
 
-        console.log(field);
-        return field;
+        }
+
+        console.log(newField)
+        return newField;
     }
+
 
     solution(field, moves);
 })();
